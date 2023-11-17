5c37b9bd01d1b4e195254ab129ce4a3238681a0e
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-11-17 18:36:30 +0530

    first app

---
+++ 02- First App/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>please! login.</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listing on Port ${PORT}`);
+});
