e2a6d4d0c0ff7392c2d95a0d8b1645ff5f38d1e0
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-12-28 00:38:41 +0530

    models

--- 03- Connect Backend/frontend/src/App.jsx
+++ 03- Connect Backend/frontend/src/App.jsx
@@ -1,23 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [jokes, setJokes] = useState([])
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/jokes")
+      .then((response) => {
+        console.log(response);
+        setJokes(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <>
       <h1>Hello, Vaibhav</h1>
       <p>Jokes: {jokes.length}</p>
-      {
-        jokes.map((joke) => (
-          <div key={joke.id}>
-            <h3>{joke.title}</h3>
-            <p>{joke.content}</p>
-          </div>
-        ))
-      }
+      {jokes.map((joke) => (
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
