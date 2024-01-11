0734535eaabfdbe348dafee12796d1422fc3ad90
vaibhavwxyz <vaibhawxyz@gmail.com>
2024-01-11 18:51:52 +0530

    utils

--- 04- YouTube Backend/backend/src/index.js
+++ 04- YouTube Backend/backend/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
@@ -9,6 +10,10 @@ const PORT = process.env.PORT || 8000;
 
 connectDB()
   .then(() => {
+    app.on("error", (error) => {
+      console.log("ERR: ", error);
+      throw error;
+    });
     app.listen(PORT, () => {
       console.log(`Server is running at PORT at ${PORT}`);
     });
